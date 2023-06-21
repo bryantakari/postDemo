6014ce89479677c3d95629867fa2fc646e26001d
Bryan Takari <bryantakari2018@gmail.com>
2023-06-21 23:16:04 +0700

    remove unused code

--- src/main/java/com/post/demo/controller/MessageController.java
+++ src/main/java/com/post/demo/controller/MessageController.java
@@ -1,14 +1,8 @@
 package com.post.demo.controller;
 
 import com.post.demo.manager.MessageManager;
-import com.post.demo.model.Message;
-import com.post.demo.model.User;
-import com.post.demo.repository.UserRepository;
 import com.post.demo.request.MessageRequest;
 import com.post.demo.response.MessageResponse;
-import com.post.demo.scheduler.BizOrderTask;
-import com.post.demo.util.ErrorContextEnum;
-import com.post.demo.util.MessageTypeEnum;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,12 +10,10 @@ import org.springframework.context.annotation.ImportResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
-import org.springframework.scheduling.support.CronTrigger;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Optional;
 
 @RestController
 @ImportResource("classpath:scheduler.xml")
@@ -32,11 +24,8 @@ public class MessageController {
     ThreadPoolTaskScheduler taskScheduler;
 
     @Autowired
-    UserRepository userRepository;
-
-    @Autowired
     MessageManager messageManager;
-    private static final String MESSAGE_SCHEDULED = "SCHEDULED";
+
     @PostMapping("/message/schedule")
     public ResponseEntity<MessageResponse> scheduleMessage(@RequestBody MessageRequest request){
         return new ResponseEntity<>(messageManager.insert(request), HttpStatus.CREATED);
@@ -45,4 +34,5 @@ public class MessageController {
     public void setTaskScheduler(ThreadPoolTaskScheduler taskScheduler) {
         this.taskScheduler = taskScheduler;
     }
+
 }
--- src/main/java/com/post/demo/manager/MessageManagerImpl.java
+++ src/main/java/com/post/demo/manager/MessageManagerImpl.java
@@ -14,11 +14,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ImportResource;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.scheduling.support.CronTrigger;
-import org.springframework.util.StringUtils;
 
 import java.util.Date;
 import java.util.Optional;
